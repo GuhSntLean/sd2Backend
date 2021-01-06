6e816146b2159b1975a169008bbe065deead412e
GuhSntLean <guleandro.66@gmail.com>
2021-01-05 23:41:06 -0300

    Implementando classe order

---
+++ src/main/java/com/backend/entities/Order.java
@@ -0,0 +1,113 @@
+package com.backend.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Order implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long        id;
+    private String      address;
+    private Double      latitude;
+    private Double      longitude;
+    private Instant     moment;
+    private OrderStatus status;
+    private Double      total;
+    //Para não repetir produtos esta sendo utilizado o parametro SET
+    private Set<Product> products = new HashSet<>();
+
+    Order(){}
+
+    public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status, Double total) {
+        super();
+        this.id = id;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.moment = moment;
+        this.status = status;
+        this.total = total;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getLatitude() {
+        return this.latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return this.longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Instant getMoment() {
+        return this.moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Double getTotal() {
+        return this.total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Set<Product> getProducts() {
+        return this.products;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        return id != null ? id.equals(order.id) : order.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
+
