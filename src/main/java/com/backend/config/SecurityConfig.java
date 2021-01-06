bf84f13a12bdfe1fd72be96d352a70369fb90c2c
GuhSntLean <guleandro.66@gmail.com>
2021-01-05 23:02:39 -0300

    Configuracao inicial feita

--- src/main/java/com/backend/config/SecurityConfig.java
+++ src/main/java/com/backend/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.backend.config;
 
 import java.util.Arrays;
-
+//Bibliotecas importadas
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
