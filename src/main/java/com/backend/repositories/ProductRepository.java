05db45546012bad8bf03d9d0b3f9e0e0f0ed5ada
GuhSntLean <guleandro.66@gmail.com>
2021-01-06 10:10:42 -0300

    GET products completo

--- src/main/java/com/backend/repositories/ProductRepository.java
+++ src/main/java/com/backend/repositories/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.backend.repositories;
 
+import java.util.List;
+
 import com.backend.entities.Product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+public interface ProductRepository extends JpaRepository<Product, Long> {
+  List<Product> findAllByOrderByNameAsc();
+}
