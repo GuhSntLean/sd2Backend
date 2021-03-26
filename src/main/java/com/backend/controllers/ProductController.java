64f5ce76d1ad51ab9ebe1da984845dcbc962877b
GuhSntLean <guleandro.66@gmail.com>
2021-03-25 23:11:55 -0300

    feat: refactory security allow access

--- src/main/java/com/backend/controllers/ProductController.java
+++ src/main/java/com/backend/controllers/ProductController.java
@@ -7,11 +7,13 @@ import com.backend.services.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/products")
 public class ProductController {
   @Autowired
